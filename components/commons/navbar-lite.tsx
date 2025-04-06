4f3885d23cbc6a8581a35b06ff74bd4e97030e4f
Roberto Cocco <robertococco@outlook.com>
2025-04-06 13:18:39 +0200

    Refactor theme toggle

--- components/commons/navbar-lite.tsx
+++ components/commons/navbar-lite.tsx
@@ -1,16 +1,12 @@
 "use client"
 
-import { ChevronLeft, Moon, Sun } from "lucide-react"
+import { ChevronLeft } from "lucide-react"
 import Link from "next/link"
 import * as React from "react"
 
-import { Button } from "@/components/ui/button"
-import { useTheme } from "next-themes"
-
+import { ThemeToggle } from "@/components/ui/theme-toggle"
 
 export default function NavbarLite({ title }: any) {
-    const { setTheme, theme } = useTheme()
-
     return (
         <nav className="fixed top-0 z-50 flex w-full items-center justify-between border-b bg-background px-6 py-5">
             <div className="mx-auto flex w-full max-w-[800px] items-center justify-between">
@@ -24,11 +20,7 @@ export default function NavbarLite({ title }: any) {
                     {title?.toLowerCase()}
                 </div>
                 <div className="flex items-center gap-6">
-                    <Button variant="ghost" size="icon" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-                        <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                        <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                        <span className="sr-only">Toggle theme</span>
-                    </Button>
+                    <ThemeToggle />
                 </div>
             </div>
         </nav>
--- components/commons/navbar.tsx
+++ components/commons/navbar.tsx
@@ -1,18 +1,15 @@
 "use client"
 
-import { ChevronRight, Menu, Moon, Sun } from "lucide-react"
+import { ChevronRight, Menu } from "lucide-react"
 import Link from "next/link"
 import * as React from "react"
 
 import { Button } from "@/components/ui/button"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import { ThemeToggle } from "@/components/ui/theme-toggle"
 import { navigation } from "@/data/navigation"
-import { useTheme } from "next-themes"
-
 
 export default function Navbar() {
-    const { setTheme, theme } = useTheme()
-
     return (
         <nav className="fixed top-0 z-50 flex w-full items-center justify-between border-b bg-background px-6 py-5">
             <div className="mx-auto flex w-full max-w-[800px] items-center justify-between">
@@ -45,11 +42,7 @@ export default function Navbar() {
                             </div>
                         </SheetContent>
                     </Sheet>
-                    <Button variant="ghost" size="icon" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-                        <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                        <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                        <span className="sr-only">Toggle theme</span>
-                    </Button>
+                    <ThemeToggle />
                 </div>
             </div>
         </nav>
